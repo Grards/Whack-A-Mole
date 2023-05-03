58dc79594a2ebff10bac3d023887900f01357db0
Steve Grard <grard.steve@gmail.com>
2023-05-03 09:27:24 +0200

    Rework of the button start and html less modifications

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,10 @@
 import { randomNumberOfMonsters } from "./modules/randomMonster.js";
 
-randomNumberOfMonsters();
+
+const start = document.getElementById("button__start")
+start.addEventListener('click', (e) => {
+    randomNumberOfMonsters()
+    start.setAttribute('disabled', true)
+    start.classList.add('button__start--disabled')
+    start.textContent = "Started"
+})
